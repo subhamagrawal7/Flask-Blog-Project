dfd0b4c0465ef70815781e2c72684c9e91d6d066
subhamagrawal7 <subhamagrawal7@gmail.com>
2020-03-14 12:22:49 +0530

    Connection with database using SQLAlchemy - Refere for notes in the practice.py file - Install the SQLAlchemy package using pip install flask-sqlalchemy - Currently all the models(Users and Posts) have been created inside the flaskblog.py file - The database URI specified creates a site.db file in the current directory's relative path

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,42 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 # To prevent from various attacks
 app.config['SECRET_KEY'] = '967d1767432f72c86aab457ed23dcf7d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+# Create user model (or user table)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # One-to-many relationship between users and posts. The lazy attribute defines when the SQL Alchemy loads the data from the database as necessary in one go
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # In above Post capital as class being referenced. Here we are referencing the users table and column
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}','{self.date_posted}')"
+
 
 posts = [
     {
--- practice.py
+++ practice.py
@@ -64,3 +64,70 @@ age = 74
 f"Hello, {name}. You are {age}."
 'Hello, Eric. You are 74.'
 '''
+
+'''
+SQLAlchemy is an ORM - Object Relational Mapper which allows us to access the database in an easy to use object
+oriented manner. It also allows to use different databases without changing the Python code. So if you want to use
+SQLite Database for testing and Postgres database for production then all you need to do is pass in a different 
+database URL for SQLAlchemy to connect to, but all the code to create the database will be the same.
+'''
+
+'''
+The following can be run in the command line for testing the database and running some queries 
+
+$python
+Python 3.7.4 (default, Aug 13 2019, 20:35:49) 
+[GCC 7.3.0] :: Anaconda, Inc. on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from flaskblog import db 
+/home/subham/anaconda3/lib/python3.7/site-packages/flask_sqlalchemy/__init__.py:835: FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning.
+  'SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and '
+>>> db.create_all()
+>>> from flaskblog import User, Post
+>>> user_1 = User(username='Subham',email='subham@demo.com',password='password')
+>>> db.session.add(user_1)
+>>> user_2 = User(username='JohnDoe',email='jd@demo.com',password='password')
+>>> db.session.add(user_2)
+>>> db.session.commit()
+>>> User.query.all()
+[User('Subham','subham@demo.com','default.jpg'), User('JohnDoe','jd@demo.com','default.jpg')]
+>>> User.query.first()
+User('Subham','subham@demo.com','default.jpg')
+>>> User.query.filter_by(username='Subham').all()
+[User('Subham','subham@demo.com','default.jpg')]
+>>> User.query.filter_by(username='Subham').first()
+User('Subham','subham@demo.com','default.jpg')
+>>> user = User.query.filter_by(username='Subham').first()
+>>> user
+User('Subham','subham@demo.com','default.jpg')
+>>> user.id
+1
+>>> user = User.query.get(1)
+>>> user
+User('Subham','subham@demo.com','default.jpg')
+>>> user.posts
+[]
+>>> user.id
+1
+>>> posts_1 = Post(title='Blog 1',content='First Post Content!',user_id=user.id)
+>>> posts_2 = Post(title='Blog 2',content='Second Post Content!',user_id=user.id)
+>>> db.session.add(posts_1)
+>>> db.session.add(posts_2)
+>>> db.session.commit()
+>>> user.posts
+[User('Blog 1','2020-03-14 06:45:03.904100'), User('Blog 2','2020-03-14 06:45:03.906327')]
+>>> for post in user.posts:
+...     print(post.title)
+... 
+Blog 1
+Blog 2
+>>> post = Post.query.first()
+>>> post
+User('Blog 1','2020-03-14 06:45:03.904100')
+>>> post.user_id
+1
+>>> post.author
+User('Subham','subham@demo.com','default.jpg')
+>>> db.drop_all() #This was used to delete all table and all rows
+>>> db.create_all() #This would have created the table structure as original
+'''
